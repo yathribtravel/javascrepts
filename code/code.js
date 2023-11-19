4c8e86b06b7ac2a2d32c0ba721007d06914625d6
test2a2a <149164071+test2a2a@users.noreply.github.com>
2023-11-19 18:39:44 +0200

    day_1.2_19_11_2023

--- code/code.js
+++ code/code.js
@@ -125,6 +125,58 @@ When to Use var, let, or const?
     3. Always use const if the type should not be changed (Arrays and Objects)
     4. Only use let if you can't use const
     5. Only use var if you MUST support old browser
+
+
+
+===============================================
+-let (Block Scope)
+  The let keyword was introduced in ES6 (2015)
+  Variables defined with let cannot be Redeclared
+  Variables defined with let must be Declared before use
+  Variables defined with let have Block Scope
+  Variables declared inside a { } block cannot be accessed from outside the block:
+  let and const does not bind to this.
+  let and const are not hoisted.
+
+  -var
+  Variables defined with let cannot be declared 
+  Variables defined with let must be Declared before use
+  Variables defined with let have not Block Scope
+  Variables declared inside a { } block cannot be accessed from outside the block:
+
+  -const
+  Variables defined with const cannot be Redeclared
+  Variables defined with const cannot be Reassigned
+  Variables defined with const have Block Scope
+
+  var x = "John Doe";  var x = 0;  #yes
+------------------
+
+  var x = 2;   // Allowed
+  let x = 3;   // Not allowed
+
+  {
+  let x = 2;   // Allowed
+  let x = 3;   // Not allowed
+  }
+
+  {
+  let x = 2;   // Allowed
+  var x = 3;   // Not allowed
+  }
+------------------  
+let x = 2;   // Allowed
+
+{
+let x = 3;   // Allowed
+}
+
+{
+let x = 4;    // Allowed
+} 
+
+
+------------------
 ===============================================
 JavaScript  is Lower Camel Case  
 
@@ -152,7 +204,26 @@ https://www.w3schools.com/charsets/ref_html_utf8.asp
 
 ===============================================
 
+const cars = ["Saab", "Volvo", "BMW"];
 
+// Change an element:
+cars[0] = "Toyota";
+
+// Add an element:
+cars.push("Audi");
+
+cars = ["Toyota", "Volvo", "Audi"];    // ERROR  ( NOT reassign the array)
+
+===============================================
+
+// You can create a const object:
+const car = {type:"Fiat", model:"500", color:"white"};
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson"; 
 
 
 
@@ -161,13 +232,41 @@ https://www.w3schools.com/charsets/ref_html_utf8.asp
 ===============================================
 
 
+    Arithmetic Operators
+     - ** 	Exponentiation 
+     - ++   Increment
+     - --   Decrement
+    Assignment Operators
+       x += 5;
+    Comparison Operators
+    -  == 	equal to
+    -  === 	equal value and equal type
+    -  ? 	ternary operator
+
+    String Operators
+
+    Logical Operators
+    -&& 	logical and
+    -|| 	logical or
+    -!   	logical not
+
+
+    Bitwise Operators
+    Operator 	Description 	Example 	Same as 	      Result 	Decimal
+        & 	     AND 	       5 & 1 	   0101 & 0001    	0001 	 1
+        | 	      OR 	       5 | 1 	    0101 | 0001 	  0101 	 5
+        ~ 	      NOT 	      ~ 5    	 ~0101 	          1010 	 10
+        ^ 	     XOR 	        5 ^ 1    	0101 ^ 0001   	0100 	 4
+        <<     	left shift  	5 << 1 	 0101 << 1      	1010 	 10
+        >>    	right shift 	5 >> 1 	 0101 >> 1      	0010 	  2
+        >>> 	unsigned right shift 	5 >>> 1 	0101 >>> 1 	0010 	  2
 
 
 
-
-
-===============================================
-
+    Ternary Operators
+    Type Operators
+    -  typeof 	Returns the type of a variable
+    -  instanceof 	Returns true if an object is an instance of an object type
 
 
 
