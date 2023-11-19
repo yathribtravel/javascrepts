59a3e59df27fe8f6dc6d158cde4516cbb1059ef9
yathribtravel <yathribtravelsystem@gmail.com>
2023-11-19 14:12:44 +0200

    day_1_19_11_2023

---
+++ code/code.js
@@ -0,0 +1,201 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+
+
+
+let x= 10
+
+
+
+document.write(x)
+x=20+10
+document.write(x)
+x=20
+  
+
+
+{/* 
+
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML=Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+===============================================
+
+
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+===============================================
+
+      <button onclick="document.getElementById('myImage').src='/img/pic_bulbon.gif'">Turn on the light</button>
+
+    <img id="myImage" src="/img/pic_bulboff.gif" style="width:100px">
+    
+    <button onclick="document.getElementById('myImage').src='/img/pic_bulboff.gif'">Turn off the light</button>
+    
+  ===============================================   
+
+
+   document.getElementById("demo").style.fontSize = "35px";
+   document.getElementById("demo").style.display = "none";
+    
+  ===============================================  
+<p id="demo" style="display:none">Hello JavaScript!</p>
+
+<button type="button" onclick="document.getElementById('demo').style.display='block'">Click Me!</button>
+
+===============================================
+
+
+<p id="demo"></p>
+
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
+
+========================myFunction=======================
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+</script>
+</head>
+<body>
+
+<h2>Demo JavaScript in Head</h2>
+
+<p id="demo">A Paragraph.</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+
+document.write(5 + 6);      # Using document.write() after an HTML document is loaded, will delete all existing HTML:
+                            # The document.write() method should only be used for testing.
+
+document.getElementById("demo").innerHTML = 5 + 6;
+window.alert(5 + 6);
+alert(5 + 6);
+console.log(5 + 6)         # debugging purposes,
+window.print()
+
+<h2>My First Web Page</h2>  #will delete all existing HTML
+<p>My first paragraph.</p>
+<button type="button" onclick="document.write(5 + 6)">Try it</button>
+===============================================
+ let a, b, c;  // Declare 3 variables
+ a = 5; b = 6; c = a + b; 
+ let person = "John Doe", carName = "Volvo", price = 200; 
+
+
+#On the web, you might see examples without semicolons.
+#Ending statements with semicolon is not required, but highly recommended.
+
+
+
+===============================================
+https://www.w3schools.com/js/js_reserved.asp
+Automatically
+var 	Declares a variable       Variable values 
+let 	Declares a block variable Fixed values
+const 	Declares a block constant
+
+x = 5;
+y = 6;
+z = x + y;
+
+var x = 5;
+var y = 6;
+var z = x + y;
+
+constant  values and cannot be changed.
+
+$("p"); means "select all p elements".
+
+When to Use var, let, or const?
+    1. Always declare variables
+    2. Always use const if the value should not be changed
+    3. Always use const if the type should not be changed (Arrays and Objects)
+    4. Only use let if you can't use const
+    5. Only use var if you MUST support old browser
+===============================================
+JavaScript  is Lower Camel Case  
+
+
+Hyphens: first-name, last-name, master-card, inter-city.
+Underscore:first_name, last_name, master_card, inter_city.
+Upper Camel Case (Pascal Case):FirstName, LastName, MasterCard, InterCity.
+Lower Camel Case: firstName, lastName, masterCard, interCity.
+===============================================
+
+
+
+
+
+
+
+===============================================
+https://www.w3schools.com/js/js_reserved.asp
+https://www.w3schools.com/charsets/ref_html_utf8.asp
+
+
+
+
+
+
+===============================================
+
+
+
+
+
+
+
+===============================================
+
+
+
+
+
+
+
+===============================================
+
+
+
+
+
+
+
+===============================================
+
+
+
+
+
+
+
+===============================================
+
+
+
+
+
+
+
+===============================================
+
+*/}
+
+
+// /*  */
+
+
+
